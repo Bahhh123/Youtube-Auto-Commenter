ecda134f85c6e43b10cd3f8e980a8007e8981784
Bahhh123 <88037809+Bahhh123@users.noreply.github.com>
2022-03-18 21:05:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  keywords: ["konflik russia","russia dan ukraina","presiden putin"],
+  keywords: ["Imls untuk MlBB terbaru"],
   comments: ["Holla oneee-chan daisuki","let's be a friend","subs to subs ahaha","oyyy kyomassaaa"],
   usernamegoogle: "awspp016",
   passwordgoogle: "Suprafit",
